f4ba5186365a5e317a1f4ec9000dd07dd69f1bed
Ramiro José Flores Acevedo <rflores@echamorro.com>
2021-08-17 17:49:23 -0600

    Prueba1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import DatePicker from "react-datepicker"
+import 'react-datepicker/dist/react-datepicker.css'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [selectedDate, setSelectedDate] = useState(null)
+    return (
+
+    <div class="container">
+    <form action="">
+      <label for="firstnames">Nombres</label>
+      <input type="text" id="firstnames" name="firstnames" placeholder="Tus nombres..." />
+  
+      <label for="lastnames">Apellidos</label>
+      <input type="text" id="lastnames" name="lastnames" placeholder="Tus apellidos..."/>
+
+      <label for="birthday">Fecha de Nacimiento</label>
+      <DatePicker id="birthday" name="birthday" selected={selectedDate} onChange={date => setSelectedDate(date)} format="MM/dd/yyyy" placeholder="Fecha de Nacimiento..." ></DatePicker>
+  
+      <label for="email">Correo electrónico</label>
+      <input type="text" id="email" name="email" placeholder="Tu correo electrónico..."/>
+  
+      <label for="telephone">Número de teléfono</label>
+      <input type="text" id="telephone" name="telephone" placeholder="Tu teléfono..."/>
+      
+      <input type="submit" value="Submit"/>
+    </form>
+  </div>
+  
   );
 }
 
